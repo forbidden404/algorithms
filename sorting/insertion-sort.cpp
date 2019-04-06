8a9be32336c6b3475ce21a55212c0b534a7d4987
Francisco Soares <francisco.ssn94@gmail.com>
2019-04-06 15:22:32 -0300

    Add function to change order of insertion sort

--- sorting/insertion-sort.cpp
+++ sorting/insertion-sort.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
-void insertSort(vector<int>& arr) {
+typedef bool (*cmp) (int a, int b);
+
+bool decreasing(int a, int b) {
+    return a < b;
+}
+
+bool increasing(int a, int b) {
+    return a > b;
+}
+
+void insertSort(vector<int>& arr, cmp f) {
     for (int j = 1; j < arr.size(); j++) {
         int key = arr[j];
         int i = j - 1;
-        for (; i >= 0 && arr[i] > key; i--) {
+        for (; i >= 0 && f(arr[i], key); i--) {
             arr[i + 1] = arr[i];
         }
         arr[i + 1] = key;
@@ -26,7 +36,11 @@ int main() {
 
     printVector(v);
 
-    insertSort(v);
+    insertSort(v, increasing);
+
+    printVector(v);
+
+    insertSort(v, decreasing);
 
     printVector(v);
 
